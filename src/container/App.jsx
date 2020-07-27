ab3f708221e71e6b1588ca935214d7e30d198dfc
Arrettino <batukearrettino@gmail.com>
2020-07-27 14:02:38 -0300

    implementation of fake-api

--- src/container/App.jsx
+++ src/container/App.jsx
@@ -5,21 +5,67 @@ import Categories from '../components/Categories';
 import Carousel from '../components/Carousel';
 import CarouselItem from '../components/CarouselItem';
 import Footer from '../components/Footer';
+import useInitialState from '../hooks/useInitialState';
 import '../assets/styles/App.scss';
 
 function App() {
+  const API = 'http://localhost:3000/initalState';
+  const videos = useInitialState(API);
   return (
     <>
       <Header />
       <Search />
-      <Categories>
-        <Carousel>
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-          <CarouselItem />
-        </Carousel>
-      </Categories>
+      {videos.mylist.length > 0 && (
+        <Categories title='My List'>
+          <Carousel>
+            {videos.mylist.map(
+              (item) => (
+                <CarouselItem
+                  key={item.id}
+                  cover={item.cover}
+                  year={item.year}
+                  contentRating={item.contentRating}
+                  duration={item.duration}
+                />
+              ),
+            )}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.trends.length > 0 && (
+        <Categories title='Trends'>
+          <Carousel>
+            {videos.trends.map(
+              (item) => (
+                <CarouselItem
+                  key={item.id}
+                  cover={item.cover}
+                  year={item.year}
+                  contentRating={item.contentRating}
+                  duration={item.duration}
+                />
+              ),
+            )}
+          </Carousel>
+        </Categories>
+      )}
+      {videos.originals.length > 0 && (
+        <Categories title='Originals'>
+          <Carousel>
+            {videos.originals.map(
+              (item) => (
+                <CarouselItem
+                  key={item.id}
+                  cover={item.cover}
+                  year={item.year}
+                  contentRating={item.contentRating}
+                  duration={item.duration}
+                />
+              ),
+            )}
+          </Carousel>
+        </Categories>
+      )}
       <Footer />
     </>
 
