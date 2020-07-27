99508dcff45c8f2a078ea1532dc4160058dcf9cd
Arrettino <batukearrettino@gmail.com>
2020-07-27 17:44:42 -0300

    added Register container

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../container/Home';
 import Login from '../container/Login';
+import Register from '../container/Register';
 import '../assets/styles/App.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/login' component={Login} />
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </Switch>
     </BrowserRouter>
   );
 }
