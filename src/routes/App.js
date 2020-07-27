a6b74318ee4e0ac876cfd6577e9677ebc9861b0b
Arrettino <batukearrettino@gmail.com>
2020-07-27 16:40:48 -0300

    create Login container and add historyApiFallback: true in webpack

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from '../container/Home';
+import Login from '../container/Login';
+import '../assets/styles/App.scss';
 
 function App() {
   return (
     <BrowserRouter>
       <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
     </BrowserRouter>
   );
 }
