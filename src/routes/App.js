b4a2a34bde9efaf35acfbcec70c9dd20ea537983
Arrettino <batukearrettino@gmail.com>
2020-07-27 17:58:08 -0300

    added NotFound container

--- src/routes/App.js
+++ src/routes/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from '../container/Home';
 import Login from '../container/Login';
 import Register from '../container/Register';
+import NotFound from '../container/NotFound';
 import '../assets/styles/App.scss';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route exact path='/' component={Home} />
         <Route exact path='/login' component={Login} />
         <Route exact path='/register' component={Register} />
+        <Route component={NotFound} />
       </Switch>
     </BrowserRouter>
   );
