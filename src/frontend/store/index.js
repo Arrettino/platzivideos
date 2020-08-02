2bff151fd41ae7020cfab63b8ca007578ac05c35
Arrettino <batukearrettino@gmail.com>
2020-08-01 22:19:51 -0300

    add server side rendering

--- src/frontend/store/index.js
+++ src/frontend/store/index.js
@@ -1,10 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './reducers';
-import App from './routes/App';
-
 const initialState = {
   'user': {},
   'playing': {},
@@ -171,11 +164,4 @@ const initialState = {
   ],
 };
 
-const store = createStore(reducer, initialState);
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('app'),
-);
+export default initialState;
